a450a36170d0251277510abf9d9926a98b4622d1
Nguyen Dang Manh <manhnd43@fpt.com>
2024-12-04 15:03:01 +0700

    1

--- accountservice/src/main/java/com/manhnd/accountservice/model/Account.java
+++ accountservice/src/main/java/com/manhnd/accountservice/model/Account.java
@@ -5,14 +5,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
-import org.springframework.data.relational.core.mapping.Table;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Table;
 
 @Data
 @NoArgsConstructor
 @Table(name = "accounts")
+@AllArgsConstructor
+@Entity
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
